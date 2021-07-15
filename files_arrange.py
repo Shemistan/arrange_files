129bb386ee23eecc748f1d421f5da70acbd460f0
Shemistan <shemistan1@gmail.com>
2021-07-15 23:12:07 +0300

    Автоматизировал

--- files_arrange.py
+++ files_arrange.py
@@ -21,33 +21,20 @@ import os, time, shutil, zipfile
 #
 # Файлы для работы взять из архива icons.zip - раззиповать проводником в папку icons перед написанием кода.
 # Имя целевой папки - icons_by_year (тогда она не попадет в коммит)
-#
-# Пригодятся функции:
-#   os.walk
-#   os.path.dirname
-#   os.path.join
-#   os.path.normpath
-#   os.path.getmtime
-#   time.gmtime
-#   os.makedirs
-#   shutil.copy2
-#
-# Чтение документации/гугла по функциям - приветствуется. Как и поиск альтернативных вариантов :)
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
-
 # Усложненное задание (делать по желанию)
-# Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
+# Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку
 # Основная функция должна брать параметром имя zip-файла и имя целевой папки.
-# Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
 
 
 class Sorter:
 
-    def __init__(self, file_name):
+    def __init__(self, file_name, where_to_copy):
         self.file_name = file_name
+        self.where_to_copy = where_to_copy
 
     def sorter_zip(self):
-        zip_file = 'icons.zip'
+        zip_file = self.file_name
         zfile = zipfile.ZipFile(zip_file)
         file_info_list = zfile.infolist()
         for content in file_info_list:
@@ -59,7 +46,7 @@ class Sorter:
             zfile.extract(content, directory)
 
     def create_directory_zip(self, date):
-        sorted = 'sorted'
+        sorted = self.where_to_copy
         if date[1] < 10:
             date_mon = '0' + str(date[1])
         else:
@@ -68,8 +55,8 @@ class Sorter:
         directory = os.path.join(sorted, date_year, date_mon)
         return directory
 
-    def sorter(self):
-        file_name = 'sorted'
+    def sorter_folder(self):
+        file_name = self.file_name
         for dir_file, dir_name, files in os.walk(file_name):
             for file in files:
                 dir = os.path.join(dir_file, file)
@@ -79,16 +66,20 @@ class Sorter:
                 shutil.copy2(dir, directory)
 
     def create_directory(self, date):
-        new = 'new'
-        directory = os.path.join(new, str(date[0]), str(date[1]))
+        new_folder = self.where_to_copy
+        directory = os.path.join(new_folder, str(date[0]), str(date[1]))
         if os.path.isdir(directory):
             pass
         else:
             os.makedirs(directory)
         return directory
 
+    def sorter(self):
+        if self.file_name[-4:] == '.zip':
+            self.sorter_zip()
+        else:
+            self.sorter_folder()
 
-a = Sorter(file_name='icons.zip')
-#a.sorter_zip()
+
+a = Sorter(file_name='icons', where_to_copy='New')
 a.sorter()
-
