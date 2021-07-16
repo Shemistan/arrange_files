6e2c4ad8489552388391ffcb00025c109e3adcee
Shemistan <shemistan1@gmail.com>
2021-07-16 07:14:47 +0300

    finish

--- files_arrange.py
+++ files_arrange.py
@@ -1,38 +1,28 @@
 # -*- coding: utf-8 -*-
 
 import os, time, shutil, zipfile
+"""Script for organizing files
+
+The script decomposes files from one
+folders (zip_archive - no preliminary
+unzip) by year and month to another.
+
+Input parameters: 
+folder_name ----------- folder to scan,
+where_to_copy --------- target folder.
+ 
+.sorter() ------------- start_work
+"""
 
 
-# Нужно написать скрипт для упорядочивания фотографий (вообще любых файлов)
-# Скрипт должен разложить файлы из одной папки по годам и месяцам в другую.
-# Например, так:
-#   исходная папка
-#       icons/cat.jpg
-#       icons/man.jpg
-#       icons/new_year_01.jpg
-#   результирующая папка
-#       icons_by_year/2018/05/cat.jpg
-#       icons_by_year/2018/05/man.jpg
-#       icons_by_year/2017/12/new_year_01.jpg
-#
-# Входные параметры основной функции: папка для сканирования, целевая папка.
-# Имена файлов в процессе работы скрипта не менять, год и месяц взять из времени создания файла.
-# Файлы для работы взять из архива icons.zip - раззиповать проводником в папку icons перед написанием кода.
-# Имя целевой папки - icons_by_year (тогда она не попадет в коммит)
-# Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
-# Усложненное задание (делать по желанию)
-# Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку
-# Основная функция должна брать параметром имя zip-файла и имя целевой папки.
+class SorterFiles:
 
-
-class Sorter:
-
-    def __init__(self, file_name, where_to_copy):
-        self.file_name = file_name
+    def __init__(self, folder_name, where_to_copy):
+        self.folder_name = folder_name
         self.where_to_copy = where_to_copy
 
     def sorter_zip(self):
-        zip_file = self.file_name
+        zip_file = self.folder_name
         zfile = zipfile.ZipFile(zip_file)
         file_info_list = zfile.infolist()
         for content in file_info_list:
@@ -54,7 +44,7 @@ class Sorter:
         return directory
 
     def sorter_folder(self):
-        file_name = self.file_name
+        file_name = self.folder_name
         for dir_file, dir_name, files in os.walk(file_name):
             for file in files:
                 dir = os.path.join(dir_file, file)
@@ -73,7 +63,7 @@ class Sorter:
         return directory
 
     def sorter(self):
-        if self.file_name[-4:] == '.zip':
+        if self.folder_name[-4:] == '.zip':
             self.sorter_zip()
         else:
             self.sorter_folder()
